db2a719201306991b31627b99edd8cfd91e4638b
Elise Segal <frc4327@gmail.com>
2026-04-04 10:54:33 -0400

    Added third motor for shooter.

--- src/main/java/frc/robot/subsystems/FuelShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelShooterSubsystem.java
@@ -25,13 +25,17 @@ public class FuelShooterSubsystem extends SubsystemBase {
     String className = this.getClass().getSimpleName();
     private SparkFlex FuelShooterMotor = new SparkFlex(RobotConstants.FuelShooterMotorCANid, MotorType.kBrushless);
     private SparkFlex FuelShooterMotor2 = new SparkFlex(RobotConstants.FuelShooterMotor2CANid, MotorType.kBrushless);
+    private SparkFlex FuelShooterMotor3 = new SparkFlex(RobotConstants.FuelShooterMotor3CANid, MotorType.kBrushless);
     private SparkClosedLoopController FuelShooterMotorLoop = FuelShooterMotor.getClosedLoopController();
-    private SparkClosedLoopController FuelShooterMotorLoop2 = FuelShooterMotor2.getClosedLoopController();;
+    private SparkClosedLoopController FuelShooterMotorLoop2 = FuelShooterMotor2.getClosedLoopController();
+    private SparkClosedLoopController FuelShooterMotorLoop3 = FuelShooterMotor3.getClosedLoopController();;
     private SparkFlexConfig FuelShooterMotorConfig = new SparkFlexConfig();
     private SparkFlexConfig FuelShooterMotorConfig2 = new SparkFlexConfig();
+    private SparkFlexConfig FuelShooterMotorConfig3 = new SparkFlexConfig();
     private RelativeEncoder FuelShooterEncoder = FuelShooterMotor.getEncoder();
     private RelativeEncoder FuelShooterEncoder2 = FuelShooterMotor2.getEncoder();
-   
+    private RelativeEncoder FuelShooterEncoder3 = FuelShooterMotor3.getEncoder();
+
     public double MaxVelocity =  RobotConstants.FuelShooterMaxVelocity; // rotations
     public double FuelShooterVelocity = 0;
     //private double FuelShooterVelocity2 = FuelShooterEncoder2.getVelocity(); not needed because both motors are mechanically linked and should have the same velocity, we can just use one encoder to get the velocity of both motors.
@@ -72,19 +76,34 @@ public class FuelShooterSubsystem extends SubsystemBase {
             // .kCosRatio(1)
         
             .sva(0, 0, 0, ClosedLoopSlot.kSlot1); // slot 1
+        
+        FuelShooterMotorConfig3
+        .closedLoop
+          .pid(0.00005, 0, 0) // slot 0
+          .pid(0, 0, 0, ClosedLoopSlot.kSlot1) // slot 1
+          .feedForward
+            .kS(0.0) // slot 0 by default
+            .kV(0.0019, ClosedLoopSlot.kSlot0) // slot 0 explicitly
+            .kA(0.0)
+            // .kG(0) // Only use one of kG and kCos
+            // .kCos(0)
+            // .kCosRatio(1)
+        
+            .sva(0, 0, 0, ClosedLoopSlot.kSlot1); // slot 1
 
         FuelShooterMotorConfig.closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
 
         FuelShooterMotorConfig2.closedLoop
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
-        
 
+        FuelShooterMotorConfig3.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+        
         // FuelShooterMotorConfig.closedLoop.maxMotion.cruiseVelocity(2000);
         // FuelShooterMotorConfig.closedLoop.maxMotion.maxAcceleration(1000);
         // FuelShooterMotorConfig2.closedLoop.maxMotion.cruiseVelocity(2000);
         // FuelShooterMotorConfig2.closedLoop.maxMotion.maxAcceleration(1000);
-
         
         // SetupMotorConfig();
          FuelShooterMotorConfig.encoder //https://www.chiefdelphi.com/t/psa-rev-spark-default-velocity-filtering-is-still-really-bad-for-flywheels/514567/2
@@ -95,24 +114,31 @@ public class FuelShooterSubsystem extends SubsystemBase {
          .uvwMeasurementPeriod(8)
          .quadratureAverageDepth(2)
          .quadratureMeasurementPeriod(8);
+         FuelShooterMotorConfig3.encoder //https://www.chiefdelphi.com/t/psa-rev-spark-default-velocity-filtering-is-still-really-bad-for-flywheels/514567/2
+         .uvwMeasurementPeriod(8)
+         .quadratureAverageDepth(2)
+         .quadratureMeasurementPeriod(8);
         //FuelShooterMotorConfig.inverted(true); // invert the first motor because of how the motors are mounted we want Shooting to be a positive velocity not -3300 (its a semantic change really but it makes it easier to understand when we are trying to shoot at a certain velocity, we can just set the velocity to a positive number instead of a negative number)
         //FuelShooterMotorConfig.encoder.quadratureMeasurementPeriod(10).quadratureAverageDepth(2); //https://www.chiefdelphi.com/t/psa-rev-spark-default-velocity-filtering-is-still-really-bad-for-flywheels/514567/2
         //FuelShooterMotorConfig2.follow(FuelShooterMotor, false); // set the second motor to follow the first motor, and dont invert it because of how the motors are mounted.
         
         FuelShooterMotor.configure(FuelShooterMotorConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
         FuelShooterMotor2.configure(FuelShooterMotorConfig2, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+        FuelShooterMotor3.configure(FuelShooterMotorConfig3, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
 
         FuelShooterEncoder.setPosition(0);
         FuelShooterEncoder2.setPosition(0);
+        FuelShooterEncoder3.setPosition(0);
     }
 
     public void stop() {
     //     //same thing but we are using .set(0);
         FuelShooterMotor.stopMotor();
         FuelShooterMotor2.stopMotor();
+        FuelShooterMotor3.stopMotor();
         FuelShooterMotor.set(0);
         FuelShooterMotor2.set(0);
-        // FuelShooterMotor3.set(0);
+        FuelShooterMotor3.set(0);
 
      }
     public void shooterOn(double velocity){
@@ -120,6 +146,8 @@ public class FuelShooterSubsystem extends SubsystemBase {
         // FuelShooterMotor.set(0);
         // FuelShooterMotor2.set(0);
         FuelShooterMotorLoop2.setSetpoint(velocity, ControlType.kVelocity);
+        FuelShooterMotorLoop3.setSetpoint(velocity, ControlType.kVelocity);
+
         FuelShooterTargetVelocity = velocity;
     }
 
@@ -134,16 +162,19 @@ public class FuelShooterSubsystem extends SubsystemBase {
     public void shooterSpeed(double power){
       FuelShooterMotor.set(power);
       FuelShooterMotor2.set(power);
+      FuelShooterMotor3.set(power);
     }
 
     public void shooterBangBang(double power){
       if (FuelShooterEncoder.getVelocity() < (1000 * 0.98)) {
         FuelShooterMotor.set(power);
         FuelShooterMotor2.set(power);
+        FuelShooterMotor3.set(power);
       }
       else {
         FuelShooterMotor.set(0);
         FuelShooterMotor2.set(0);
+        FuelShooterMotor3.set(0);
       }
     }
 
